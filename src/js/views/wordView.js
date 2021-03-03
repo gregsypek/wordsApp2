00af197971901677883a045d88a1482be4b57e56
greg <greg.sypek@gmail.com>
2021-03-03 19:45:32 +0100

    change layout for aside list

--- src/js/views/wordView.js
+++ src/js/views/wordView.js
@@ -19,45 +19,64 @@ class WordView extends View {
     return `
      <div class="aside__results">
           <ul class="aside__list">
-           <a href="#soup">soup</a>
-            <a href="#cut">cut</a> -->
-      <li class="aside__item ">
-        <a href="${this._data.word}" class="aside__link">
-          <strong class="aside__link--eng">${this._data.word}</strong>&nbsp;
-
-          <dfn class="aside__link--data">
-            ${this._data.meanings
-              .map(item => {
-                return `
-               <span class="aside__link--type">
-            ${item.partOfSpeech}
-
-            ${item.definitions.map(this._generateMarkupDefinitions).join('')}
-              </span>
-              `;
-              })
-              .join('')}
-           
-          </dfn>        
-        </a>
-         <button class="btn--plus-sm hidden">
-                <svg class="nav__icon">
-                  <use href="${icons}#icon-plus"></use>
-                </svg>
-              </button>
-      </li>
-      </ul>
-      </div>
+            <li class="aside__item">
+              <strong class="aside__link--eng">${this._data.word}</strong>&nbsp;
+              ${this._data.meanings
+                .map(item => {
+                  return `
+                <div class="aside__link">
+                <dfn class="aside__link--data">
+                  <span class="aside__link--type">${item.partOfSpeech}</span>
+                  ${item.definitions
+                    .map(this._generateMarkupDefinitions)
+                    .join('')}
+                </dfn>
+                <button class="btn--plus-sm hidden">
+                  <svg class="nav__icon">
+                    <use href="${icons}#icon-plus"></use>
+                  </svg>
+                </button>
+              </div>
+                `;
+                })
+                .join('')}             
+         
+            </li>
+          </ul>
+          <div class="aside__footer hidden">
+            <button class="btn--page">
+              <svg class="bar__icon">
+                <use href="./src/img/icons.svg#icon-chevron-left"></use>
+              </svg>
+              <span>Page 1</span>
+            </button>
+            <button class="btn--page">
+              <span>Page 2</span>
+              <svg class="bar__icon">
+                <use href="./src/img/icons.svg#icon-chevron-right"></use>
+              </svg>
+            </button>
+            <button class="btn--print">
+              <svg class="bar__icon">
+                <use href="./src/img/icons.svg#icon-print"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
     `;
   }
 
   _generateMarkupDefinitions(def, index) {
     return `
-               <span class="aside__link--meanings">
-                <em class="aside__link--nr">${index + 1}</em>
-                <em class="aside__link--def">${def.definition}</em>
-              </span>
+              <span class="aside__link--meanings">
+                    <em class="aside__link--nr">${index + 1}</em>
+                    <em class="aside__link--def">${def.definition}</em>
+                  </span>
               `;
+  }
+  _makeWordActive(word) {
+    e.preventDefault();
+    console.log(e);
   }
 }
 
